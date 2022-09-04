241342d0232e2c02cb160e8b9168020292d7770f
Glenn Hevey <glenn.hevey@gmail.com>
2022-09-05 08:18:59 +1000

    base graphics view added to output manager

--- src/Cade.DMG/GbOutputManager.cs
+++ src/Cade.DMG/GbOutputManager.cs
@@ -1,33 +1,37 @@
 ﻿using System;
 using Cade.Common.Interfaces;
 
-namespace Cade.DMG
+namespace Cade.DMG;
+
+public class GbOutputManager : CadeOutputManager
 {
-	public class GbOutputManager : CadeOutputManager
+    private GraphicsView? graphicsView;
+    //private GraphicsCanvas graphicsCanvas;
+
+    public GbOutputManager()
 	{
-		public GbOutputManager()
-		{
-		}
+	}
 
-        public override void AddGraphicsView(GraphicsView graphicsView)
-        {
-            throw new NotImplementedException();
-        }
+    public override void AddGraphicsView(GraphicsView graphicsView)
+    {
+        this.graphicsView = graphicsView;
+        //TODO: Add Graphics Canvas
+        //this.graphicsView.Drawable = graphicsCanvas;
+    }
 
-        public override void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
 
-        public override void Draw()
-        {
-            throw new NotImplementedException();
-        }
+    public override void Draw()
+    {
+        throw new NotImplementedException();
+    }
 
-        public override void Setup()
-        {
-            throw new NotImplementedException();
-        }
+    public override void Setup()
+    {
+        throw new NotImplementedException();
     }
 }
 
