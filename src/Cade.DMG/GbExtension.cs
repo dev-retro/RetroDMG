df96bdd8272608aabc0b8c034624bc94988e47f1
Glenn Hevey <glenn.hevey@gmail.com>
2022-09-05 08:18:41 +1000

    initial extension build out

--- src/Cade.DMG/GbExtension.cs
+++ src/Cade.DMG/GbExtension.cs
@@ -1,63 +1,87 @@
 ﻿using System;
 using Cade.Common.Interfaces;
+using Cade.DMG.Core;
 
-namespace Cade.DMG
+namespace Cade.DMG;
+
+public class GbExtension : CadeExtension
 {
-	public class GbExtension : CadeExtension
-	{
-		public GbExtension() : base(new GbInputManager(), new GbOutputManager())
-		{
-		}
+    private CoreManager? coreManager;
+    private IDispatcherTimer? cpuTimer;
+    private IDispatcherTimer? graphicsTimer;
 
-        public override string ExtensionName => "GameBoy";
+    public GbExtension() : base(new GbInputManager(), new GbOutputManager())
+	{}
 
-        public override string ExtensionDescription => "Nintendo GameBoy extension for the Cade arcade system";
+    public override string ExtensionName => "GameBoy";
 
-        public override string ExtensionDeveloper => "Cade";
+    public override string ExtensionDescription => "Nintendo GameBoy extension for the Cade arcade system";
 
-        public override Guid ExtensionGuid => Guid.Parse("04c192df-279f-444f-baf4-5358e49ebdef");
+    public override string ExtensionDeveloper => "Cade";
 
-        public override string PlatformName => "Nintendo GameBoy";
+    public override Guid ExtensionGuid => Guid.Parse("04c192df-279f-444f-baf4-5358e49ebdef");
 
-        public override string PlatformDescription => "The Game Boy is an 8-bit handheld game console developed and manufactured by Nintendo.";
+    public override string PlatformName => "Nintendo GameBoy";
 
-        public override string PlatformDeveloper => "Nintendo";
+    public override string PlatformDescription => "The Game Boy is an 8-bit handheld game console developed and manufactured by Nintendo.";
 
-        public override int MaxPlayers => 1;
+    public override string PlatformDeveloper => "Nintendo";
 
-        public override DateTime ReleaseDate => new DateTime(1989, 04, 21);
+    public override int MaxPlayers => 1;
 
-        public override string[] SupportedFileExtensions => new string[] { "gb" };
+    public override DateTime ReleaseDate => new DateTime(1989, 04, 21);
 
-        public override void Close()
+    public override string[] SupportedFileExtensions => new string[] { "gb" };
+
+    public override void Close()
+    {
+        cpuTimer?.Stop();
+        graphicsTimer?.Stop();
+    }
+
+    public override void Load(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Load(byte[] file)
+    {
+        coreManager?.LoadGame(file);
+    }
+
+    public override void Run(CancellationTokenSource cancellationTokenSource)
+    {
+        if(coreManager is not null)
         {
-            throw new NotImplementedException();
-        }
+            var dispatcher = Application.Current!.Dispatcher; // if your context isn't a BindableObject (if your context is a BO then just this.Dispatcher...)
+            cpuTimer = dispatcher.CreateTimer();
+            cpuTimer.Interval = TimeSpan.FromSeconds(1.0 / 4194304);
+            cpuTimer.Tick += (s, e) =>
+            {
+                coreManager.Tick();
+            };
+            cpuTimer.Start();
 
-        public override void Load(string path)
-        {
-            throw new NotImplementedException();
-        }
 
-        public override void Load(byte[] file)
-        {
-            throw new NotImplementedException();
-        }
 
-        public override void Run(CancellationTokenSource cancellationTokenSource)
-        {
-            throw new NotImplementedException();
+            graphicsTimer = dispatcher.CreateTimer();
+            graphicsTimer.Interval = TimeSpan.FromSeconds(1.0 / 59.73);
+            graphicsTimer.Tick += (s, e) =>
+            {
+                //TODO: emulate the graphics update
+            };
+            graphicsTimer.Start();
         }
+    }
 
-        public override void Setup()
-        {
-            //TODO: when ready us this to get the extension ready.
-        }
+    public override void Setup()
+    {
+        coreManager = new();
+    }
 
-        public override void Toggle()
-        {
-            throw new NotImplementedException();
-        }
+    public override void Toggle()
+    {
+        throw new NotImplementedException();
     }
 }
 
