56a0f9c750f9f43ff813bbb1d5d353ff4412a0a7
Glenn Hevey <glenn.hevey@gmail.com>
2022-09-05 08:20:36 +1000

    Added input list

--- src/Cade.DMG/GbInputManager.cs
+++ src/Cade.DMG/GbInputManager.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Cade.Common.Interfaces;
 using Cade.Common.Models;
+#if MACCATALYST
+using UIKit;
+#endif
+
 
 namespace Cade.DMG
 {
@@ -10,19 +14,111 @@ namespace Cade.DMG
 		{
 		}
 
+        private static Lazy<List<Input>> _lazy = new(() => new List<Input>
+        {
+            new Input
+            {
+                Name = "Up",
+                Active = false,
+                Number = 1,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardW
+#endif
+            },
+            new Input
+            {
+                Name = "Down",
+                Active = false,
+                Number = 2,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardS
+#endif
+            },
+            new Input
+            {
+                Name = "Left",
+                Active = false,
+                Number = 3,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardA
+#endif
+            },
+            new Input
+            {
+                Name = "Right",
+                Active = false,
+                Number = 4,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardD
+#endif
+            },
+            new Input
+            {
+                Name = "A",
+                Active = false,
+                Number = 5,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardI
+#endif
+            },
+            new Input
+            {
+                Name = "B",
+                Active = false,
+                Number = 6,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardJ
+#endif
+            },
+            new Input
+            {
+                Name = "Start",
+                Active = false,
+                Number = 7,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardB
+#endif
+            },
+            new Input
+            {
+                Name = "Select",
+                Active = false,
+                Number = 8,
+                Player = 1,
+                Type = InputType.Button,
+#if MACCATALYST
+                Key = UIKeyboardHidUsage.KeyboardV
+#endif
+            },
+        });
+
         public override List<Input> Inputs()
         {
-            throw new NotImplementedException();
+            return _lazy.Value;
         }
 
         public override int MaxPlayers()
         {
-            throw new NotImplementedException();
+            return 1;
         }
 
         public override void Update(List<Input> inputs)
         {
-            throw new NotImplementedException();
+            _lazy = new Lazy<List<Input>>(() => inputs);
         }
     }
 }
