53eeaa8cdfb677c13cd2676b8e7e54ce1363868b
Glenn Hevey <glenn.hevey@gmail.com>
2023-03-24 20:53:48 +1100

    added flag functions

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,4 @@
 pub mod memory;
 pub mod registers;
 pub mod register_type;
+pub mod flag_type;
--- src/core/registers.rs
+++ src/core/registers.rs
@@ -1,7 +1,7 @@
-use crate::core::register_type;
+use crate::core::flag_type::FlagType;
 use crate::core::register_type::RegisterType;
 
-struct Registers {
+pub struct Registers {
     af: u16,
     bc: u16,
     de: u16,
@@ -22,7 +22,7 @@ impl Registers {
         }
     }
 
-    pub fn set(&mut self, register: RegisterType, value: u16, ) {
+    pub fn set(&mut self, register: RegisterType, value: u16) {
         match register {
             RegisterType::AF => { self.af = value; }
             RegisterType::BC => { self.bc = value; }
@@ -32,7 +32,7 @@ impl Registers {
         }
     }
 
-    pub fn get(self, register: RegisterType) -> u16 {
+    pub fn get(&self, register: RegisterType) -> u16 {
         match register {
             RegisterType::AF => { self.af }
             RegisterType::BC => { self.bc }
@@ -62,7 +62,7 @@ impl Registers {
         }
     }
 
-    pub fn get_lower(self, register: RegisterType) -> u8 {
+    pub fn get_lower(&self, register: RegisterType) -> u8 {
         match register {
             RegisterType::AF => { self.af as u8 }
             RegisterType::BC => { self.bc as u8 }
@@ -72,13 +72,107 @@ impl Registers {
         }
     }
 
-    pub fn get_higher(self, register: RegisterType) -> u8 {
+    pub fn get_higher(&self, register: RegisterType) -> u8 {
         match register {
             RegisterType::AF => { (self.af << 8) as u8 }
             RegisterType::BC => { (self.bc << 8) as u8 }
             RegisterType::DE => { (self.de << 8) as u8 }
             RegisterType::HL => { (self.hl << 8) as u8 }
             RegisterType::SP => { (self.sp << 8) as u8 }
+        }
+    }
+
+    pub fn set_flag(&mut self, flag: FlagType, set: bool) {
+        match flag {
+            FlagType::Zero => {
+                let bit: i32 = 8 % 8;
+                let mask: u8 = 1 << bit;
+
+                let mut value = self.get_lower(RegisterType::AF);
+
+                if set {
+                    value |= mask;
+                    self.set_lower(RegisterType::AF, value);
+                } else {
+                    value &= !mask;
+                    self.set_lower(RegisterType::AF, value);
+                }
+            }
+            FlagType::Subtraction => {
+                let bit: i32 = 7 % 8;
+                let mask: u8 = 1 << bit;
+
+                let mut value = self.get_lower(RegisterType::AF);
+
+                if set {
+                    value |= mask;
+                    self.set_lower(RegisterType::AF, value);
+                } else {
+                    value &= !mask;
+                    self.set_lower(RegisterType::AF, value);
+                }
+            }
+            FlagType::HalfCarry => {
+                let bit: i32 = 6 % 8;
+                let mask: u8 = 1 << bit;
+
+                let mut value = self.get_lower(RegisterType::AF);
+
+                if set {
+                    value |= mask;
+                    self.set_lower(RegisterType::AF, value);
+                } else {
+                    value &= !mask;
+                    self.set_lower(RegisterType::AF, value);
+                }
+            }
+            FlagType::Carry => {
+                let bit: i32 = 5 % 8;
+                let mask: u8 = 1 << bit;
+
+                let mut value = self.get_lower(RegisterType::AF);
+
+                if set {
+                    value |= mask;
+                    self.set_lower(RegisterType::AF, value);
+                } else {
+                    value &= !mask;
+                    self.set_lower(RegisterType::AF, value);
+                }
+            }
+        }
+    }
+
+    pub fn get_flag(&self, flag: FlagType) -> u8 {
+        match flag {
+            FlagType::Zero => {
+                let bit: i32 = 8 % 8;
+                let mask: u8 = 1 << bit;
+                let value: u8 = self.get_lower(RegisterType::AF) & mask >> bit;
+
+                value
+            }
+            FlagType::Subtraction => {
+                let bit: i32 = 7 % 8;
+                let mask: u8 = 1 << bit;
+                let value: u8 = self.get_lower(RegisterType::AF) & mask >> bit;
+
+                value
+            }
+            FlagType::HalfCarry => {
+                let bit: i32 = 6 % 8;
+                let mask: u8 = 1 << bit;
+                let value: u8 = self.get_lower(RegisterType::AF) & mask >> bit;
+
+                value
+            }
+            FlagType::Carry => {
+                let bit: i32 = 5 % 8;
+                let mask: u8 = 1 << bit;
+                let value: u8 = self.get_lower(RegisterType::AF) & mask >> bit;
+
+                value
+            }
         }
     }
 }
