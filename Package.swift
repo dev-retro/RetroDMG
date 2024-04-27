b56642d4846b31ecefd6c18320a532e94bd31938
Glenn Hevey <glenn@hevey.au>
2024-04-28 07:25:05 +1000

    removed dynamic loading

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RetroDMG",
-            type: .dynamic,
             targets: ["RetroDMG"]),
     ],
     dependencies: [
