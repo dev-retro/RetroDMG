e9e3614d1ae8ec8870dac400bbac88cc23f6624c
Glenn Hevey <glenn@hevey.au>
2024-09-30 21:54:14 +1000

    Cleaned up redundant setting

--- Sources/RetroDMG/Models/DMGSettings.swift
+++ Sources/RetroDMG/Models/DMGSettings.swift
@@ -10,17 +10,10 @@ import RetroSwift
 
 public struct DMGSettings: Codable {
     public var biosSetting = BiosSetting()
-    public var gameSetting = GameSetting()
 }
 
 public struct BiosSetting: RetroSetting {
     public var name: String = "Bios"
-    public var type: RetroSettingType = .file
-    public var value: [UInt8]?
-}
-
-public struct GameSetting: RetroSetting {
-    public var name: String = "Game"
     public var type: RetroSettingType = .file
     public var value: [UInt8]?
 }
